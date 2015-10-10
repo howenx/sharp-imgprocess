73df7c16c5978e0ffd63cc5d8f9787d9e84505b5
howenkakao <howen.xiong@daumkakao.com>
2015-10-10 20:41:55 +0800

    access control

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.use(function(err, req, res, next) {
 });
 app.all('', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "");
-	res.header("Access-Control-Allow-Headers", “Content - Type, Content - Length, Authorization, Accept, X - Requested - With”);
+	res.header("Access-Control-Allow-Headers","Content - Type, Content - Length, Authorization, Accept, X - Requested - With");
 	res.header("Access - Control - Allow - Methods ", "PUT, POST, GET, DELETE, OPTIONS");
 	res.header("X - Powered - By ", "3.2.1")
 	if (req.method == "OPTIONS") res.send(200);
